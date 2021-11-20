6bcaae2f45b097a5ae6e3443e4f593f3460c6a91
Tobias Schulte to Brinke <tobias.schulte_to_brinke@yahoo.com>
2021-11-20 01:41:17 +0100

    a lot of prints for memory consumption

--- simulation/simulation_runner.py
+++ simulation/simulation_runner.py
@@ -62,22 +62,28 @@ class SimulationRunner:
             data_dir = general_utils.get_default_data_dir()
         self.results_folder = os.path.join(data_dir, 'simulation_runs', self.group_name, self.run_title)
         os.makedirs(self.results_folder, exist_ok=True)
+        general_utils.print_memory_consumption('Memory usage - end init SimulationRunner')
 
     def _create_network(self):
+        general_utils.print_memory_consumption('Memory usage - beginning _create_network')
         if self.network_type == 'brunel':
             network = brunel.BrunelNetwork(**self.network_params)
         elif self.network_type == 'alzheimers':
             network = alzheimers.AlzheimersNetwork(**self.network_params)
         else:
             raise ValueError(f'Network type unknown: "{self.network_type}"')
+        general_utils.print_memory_consumption('Memory usage - end _create_network')
 
         return network
 
     def _setup_state_recording(self):
+        general_utils.print_memory_consumption('Memory usage - beginning _setup_state_recording')
         self.network.set_up_state_multimeter(interval=self.step_duration)
         self.network.set_up_spike_filtering(interval=self.step_duration, filter_tau=20.)
+        general_utils.print_memory_consumption('Memory usage - end _setup_state_recording')
 
     def _setup_input(self):
+        general_utils.print_memory_consumption('Memory usage - beginning _setup_input')
         input_generators = None
         if 'step_' in self.input_type:
             if self.input_type == 'step_rate':
@@ -121,33 +127,46 @@ class SimulationRunner:
                 nest.Connect(generator, input_neuronlist[start:end], 'all_to_all', {'weight': self.network.J})
 
             input_generators = spatial_enc_devices
+        general_utils.print_memory_consumption('Memory usage - end _setup_input')
 
         return input_generators
 
     def _save_states(self):
+        general_utils.print_memory_consumption('\n\nMemory usage - beginning _save_states')
+
         print('copying parameters file ...')
         copyfile(self.paramfile, os.path.join(self.results_folder, 'parameters.yaml'))
+
         print('saving input signal file ...')
+        general_utils.print_memory_consumption('\tMemory usage - before saving input_signal')
         np.save(os.path.join(self.results_folder, 'input_signal.npy'), self.input_signal)
+
         print('saving Vm state matrix ...')
+        general_utils.print_memory_consumption('\tMemory usage - before saving vm_statemat')
         statemat_vm = self.network.get_statematrix()
         np.save(os.path.join(self.results_folder, 'vm_statemat.npy'), statemat_vm)
         del statemat_vm
         gc.collect()
+
         print('saving filtered spikes state matrix ....')
+        general_utils.print_memory_consumption('\tMemory usage - before saving filter_statemat')
         statemat_filter = self.network.get_filter_statematrix()
         np.save(os.path.join(self.results_folder, 'filter_statemat.npy'), statemat_filter)
         del statemat_filter
         gc.collect()
 
         print('saving spikes ...')
+        general_utils.print_memory_consumption('\tMemory usage - before saving spike_events')
         spike_events = self.spike_recorder.get('events')
         with open(os.path.join(self.results_folder, 'spike_events.pkl'), 'wb') as spikes_file:
             pickle.dump(spike_events, spikes_file)
         del spike_events
         gc.collect()
 
+        general_utils.print_memory_consumption('Memory usage - end _save_states')
+
     def _create_plots(self):
+        general_utils.print_memory_consumption('\n\nMemory usage - beginning _create_plots')
         print('creating raster plot ...')
         rasterplot_spikelist = general_utils.spikelist_from_recorder(self.spike_recorder, stop=self.raster_plot_duration)
 
@@ -165,6 +184,7 @@ class SimulationRunner:
         gc.collect()
 
         print('creating Vm state matrix plot ...')
+        general_utils.print_memory_consumption('\tMemory usage - before vm state matrix plot')
         plt.clf()
         fig = plt.figure(figsize=(12, 9))
         statemat_vm = self.network.get_statematrix()
@@ -175,24 +195,30 @@ class SimulationRunner:
         plt.title('Vm states')
         state_plot_path = os.path.join(self.results_folder, 'state_plot.pdf')
         plt.savefig(state_plot_path)
-        del statemat_vm
+        plt.clf()
+        del im
+        del statemat_vm_slice
         gc.collect()
 
         print('creating filtered spikes state matrix plot ...')
+        general_utils.print_memory_consumption('\tMemory usage - before filter state matrix plot')
         plt.clf()
         fig = plt.figure(figsize=(12, 9))
-        statemat_filter = self.network.get_filter_statematrix()
-        im = plt.matshow(statemat_filter[:, :100], fignum=fig.number, aspect='auto')
+        statemat_filter_slice = self.network.get_filter_statematrix()[:, :100].copy()
+        im = plt.matshow(statemat_filter_slice, fignum=fig.number, aspect='auto')
         plt.ylabel('neuron id')
         plt.xlabel('steps')
         plt.colorbar(im, label='filter neuron V_m')
         plt.title('filtered spikes')
         filtered_states_plot_path = os.path.join(self.results_folder, 'filtered_states_plot.pdf')
         plt.savefig(filtered_states_plot_path)
-        del statemat_filter
+        del im
+        del statemat_filter_slice
+        plt.clf()
         gc.collect()
 
         print('creating spike statistics plot ...')
+        general_utils.print_memory_consumption('\tMemory usage - before statistics plot')
         statistic_spikelist = general_utils.spikelist_from_recorder(self.spike_recorder)
         hist_data = {
             'CV': statistic_spikelist.cv_isi(),
@@ -209,9 +235,13 @@ class SimulationRunner:
 
         statistics_plot_path = os.path.join(self.results_folder, 'statistics_plot.pdf')
         plt.savefig(statistics_plot_path)
+        general_utils.print_memory_consumption('Memory usage - end _create_plots')
 
     def run(self):
+        general_utils.print_memory_consumption('Memory usage - before simulation')
         nest.Simulate(self.num_steps*self.step_duration + self.dt)
+        general_utils.print_memory_consumption('Memory usage - after simulation')
         gc.collect()
+        general_utils.print_memory_consumption('Memory usage - after simulation and garbage collection.')
         self._save_states()
         self._create_plots()
