fecc331729dc2b139a0dcfea786c55aa122edbf7
Tobias Schulte to Brinke <tobias.schulte_to_brinke@yahoo.com>
2022-10-21 17:43:49 +0200

    added create_spatial_encoding_statematrix.py

---
+++ create_spatial_encoding_statematrix.py
@@ -0,0 +1,49 @@
+import os
+import argparse
+
+import numpy as np
+import yaml
+
+from utils.input_utils import get_gaussian_input_values
+
+
+def parse_cmd():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--parameters', required=True, help='Path to the parameter file of the simulation.')
+    parser.add_argument('--inputs', required=True, help='Path to the inputs file of the simulation.')
+    parser.add_argument('--output', help='Path to store the resulting state matrix to.')
+    parser.add_argument('--N', help='Number of encoding variables.')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_cmd()
+
+    inputs = np.load(args.inputs)
+
+    with open(args.parameters, 'r') as params_file:
+        parameters = yaml.safe_load(params_file)
+
+    std = parameters['spatial_std_factor']
+    max_value = parameters['input_max_value']
+    min_value = parameters['input_min_value']
+
+    if args.N is None:
+        n_encoder = 447 if parameters['network_type'] == 'microcircuit' else 1000
+    else:
+        n_encoder = args.N
+
+    enc_state_matrix = get_gaussian_input_values(input_values=inputs, n_encoder=n_encoder, min_value=min_value,
+                                                 max_value=max_value, std=std)
+
+    if args.output is None:
+        args.output = os.path.join(os.path.dirname(args.parameters), 'encoder_state_matrix.npy')
+
+    np.save(args.output, enc_state_matrix)
+    print(f'Encoder state matrix saved to {args.output}')
+
+
+if __name__ == "__main__":
+    main()
--- utils/input_utils.py
+++ utils/input_utils.py
@@ -54,7 +54,6 @@ def _get_XOR_positions(binary_string):
 
 
 def get_gaussian_XOR_input_values(input_values, max_value, min_value, n_generators, std):
-
     # convert input values (0 ... 15) to binary strings ('0000' ... '1111')
     input_binary_strings = [f'{iv:b}'.rjust(2, '0') for iv in input_values]
 
@@ -84,8 +83,8 @@ def get_gaussian_XOR_input_values(input_values, max_value, min_value, n_generato
     return final_values
 
 
-def set_XOR_input_to_gaussian_spatial_encoder(input_values, encoding_generator, step_duration, min_value, max_value, std, start=0.):
-
+def set_XOR_input_to_gaussian_spatial_encoder(input_values, encoding_generator, step_duration, min_value, max_value,
+                                              std, start=0.):
     n_generators = len(encoding_generator)
 
     final_values = get_gaussian_XOR_input_values(input_values, max_value, min_value, n_generators, std)
@@ -95,22 +94,25 @@ def set_XOR_input_to_gaussian_spatial_encoder(input_values, encoding_generator, 
     if encoding_generator.model[0] == 'inhomogeneous_poisson_generator':
         nest.SetStatus(encoding_generator, [{'rate_times': times, 'rate_values': vals} for vals in final_values])
     elif encoding_generator.model[0] == 'step_current_generator':
-        nest.SetStatus(encoding_generator, [{'amplitude_times': times, 'amplitude_values': vals} for vals in final_values])
+        nest.SetStatus(encoding_generator,
+                       [{'amplitude_times': times, 'amplitude_values': vals} for vals in final_values])
     else:
         raise ValueError(f'device_type "{encoding_generator}" is not supported.')
 
 
 def _get_XORXOR_positions(binary_string):
-    interpolated_XORXOR_values = np.array([0.75*int(binary_string[0]), 0.25*int(binary_string[1]), -0.25*int(binary_string[2]), -0.75*int(binary_string[3])])
-    interpolated_XORXOR_values[interpolated_XORXOR_values==0] = np.nan
+    interpolated_XORXOR_values = np.array(
+        [0.75 * int(binary_string[0]), 0.25 * int(binary_string[1]), -0.25 * int(binary_string[2]),
+         -0.75 * int(binary_string[3])])
+    interpolated_XORXOR_values[interpolated_XORXOR_values == 0] = np.nan
 
     return interpolated_XORXOR_values
 
 
-def set_XORXOR_input_to_gaussian_spatial_encoder(input_values, encoding_generator, step_duration, min_value, max_value, std, start=0.):
-
+def set_XORXOR_input_to_gaussian_spatial_encoder(input_values, encoding_generator, step_duration, min_value, max_value,
+                                                 std, start=0.):
     # convert input values (0 ... 15) to binary strings ('0000' ... '1111')
-    input_binary_strings = [f'{iv:b}'.rjust(4,'0') for iv in input_values]
+    input_binary_strings = [f'{iv:b}'.rjust(4, '0') for iv in input_values]
 
     # convert binary strings ('0000' ... '1111') to 2D ndarray of positions
     # we have only the possible positions -0.75, -0.25, 0.25 and 0.75 for the 4 different input streams of the XORXOR task
@@ -123,7 +125,8 @@ def set_XORXOR_input_to_gaussian_spatial_encoder(input_values, encoding_generato
     final_values = np.zeros([len(encoding_generator), len(input_values)])
     for input_dim_index in range(array2d_input_positions.shape[1]):
         values = np.zeros([len(encoding_generator), len(input_values)])
-        interpolated_input_values = np.interp(array2d_input_positions[:, input_dim_index], (-1, 1), (0, len(encoding_generator)))
+        interpolated_input_values = np.interp(array2d_input_positions[:, input_dim_index], (-1, 1),
+                                              (0, len(encoding_generator)))
 
         # we set the inactive inputs (0) to a small value that is far out of the range of input neurons
         # then we can calculate the pdf in the normal way, without changing the values for the deactivated inputs
@@ -141,27 +144,33 @@ def set_XORXOR_input_to_gaussian_spatial_encoder(input_values, encoding_generato
     if encoding_generator.model[0] == 'inhomogeneous_poisson_generator':
         nest.SetStatus(encoding_generator, [{'rate_times': times, 'rate_values': vals} for vals in final_values])
     elif encoding_generator.model[0] == 'step_current_generator':
-        nest.SetStatus(encoding_generator, [{'amplitude_times': times, 'amplitude_values': vals} for vals in final_values])
+        nest.SetStatus(encoding_generator,
+                       [{'amplitude_times': times, 'amplitude_values': vals} for vals in final_values])
     else:
         raise ValueError(f'device_type "{encoding_generator}" is not supported.')
 
 
-def set_input_to_gaussian_spatial_encoder(input_values, encoding_generator, step_duration, min_value, max_value, std, start=0.):
-
-    interpolated_input_values = np.interp(input_values, (-1, 1), (0, len(encoding_generator)))
-    xvals = np.arange(0, len(encoding_generator)).reshape(len(encoding_generator), 1)
-
-    values = np.zeros([len(encoding_generator), len(input_values)])
+def get_gaussian_input_values(input_values, n_encoder, max_value, min_value, std):
+    interpolated_input_values = np.interp(input_values, (-1, 1), (0, n_encoder))
+    xvals = np.arange(0, n_encoder).reshape(n_encoder, 1)
+    values = np.zeros([n_encoder, len(input_values)])
     values[:] = scipy.stats.norm.pdf(xvals, interpolated_input_values[:], std)
     values /= values.max()
     values *= (max_value - min_value)
     values += min_value
+    return values
+
+
+def set_input_to_gaussian_spatial_encoder(input_values, encoding_generator, step_duration, min_value, max_value, std,
+                                          start=0.):
+    values = get_gaussian_input_values(input_values, n_encoder=len(encoding_generator), max_value=max_value,
+                                       min_value=min_value, std=std)
 
     times = start + np.arange(0.1, step_duration * len(input_values) + 0.1, step_duration)
 
     if encoding_generator.model[0] == 'inhomogeneous_poisson_generator':
         nest.SetStatus(encoding_generator, [{'rate_times': times, 'rate_values': vals} for vals in values])
     elif encoding_generator.model[0] == 'step_current_generator':
-        nest.SetStatus(encoding_generator, [{'amplitude_times': times, 'amplitude_values':vals} for vals in values])
+        nest.SetStatus(encoding_generator, [{'amplitude_times': times, 'amplitude_values': vals} for vals in values])
     else:
         raise ValueError(f'device_type "{encoding_generator}" is not supported.')
